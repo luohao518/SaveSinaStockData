9d02f09fa183e5b15c83d346f8c1b0a3bfb87ee3
luohao <luohao518@163.com>
2018-04-22 19:38:03 +0800

    自定盯盘系统基本成型

--- src/main/java/com/yilihjy/savesinastockdata/RealTimeData.java
+++ src/main/java/com/yilihjy/savesinastockdata/RealTimeData.java
@@ -16,6 +16,80 @@ import java.util.regex.Pattern;
  *
  */
 public class RealTimeData {
+
+	public static List<RealTimeDataPOJO> etRealTimeDataObjectsForFjFunds(String[] codes) {
+		String indexPatternString = "var hq_str_s_(\\w{8})=\"(.+)\"";
+		String stockPatterString = "var hq_str_(\\w{8})=\"(.+)\"";
+		Pattern indexPatter = Pattern.compile(indexPatternString);
+		Pattern stockPatter = Pattern.compile(stockPatterString);
+		List<RealTimeDataPOJO> result = new ArrayList<>();
+		String url = createURL(codes);
+		String response = Tools.sendHTTPGET(url, "GBK");
+		String[] responses = response.split(";");
+		for (int i = 0; i < responses.length; i++) {
+			String reresponseString = responses[i];
+			Matcher stockMatcher = stockPatter.matcher(reresponseString);
+			if (stockMatcher.find()) {
+				RealTimeDataPOJO obj = new RealTimeDataPOJO();
+				obj.setType(RealTimeDataPOJO.STOCK);
+				obj.setFullCode(stockMatcher.group(1));
+				String[] array = stockMatcher.group(2).split(",");
+				obj.setName(array[0]);
+				obj.setOpen(Double.parseDouble(array[1]));
+				obj.setClose(Double.parseDouble(array[2]));
+				obj.setNow(Double.parseDouble(array[3]));
+				obj.setHigh(Double.parseDouble(array[4]));
+				obj.setLow(Double.parseDouble(array[5]));
+				obj.setBuyPrice(Double.parseDouble(array[6]));
+				obj.setSellPrice(Double.parseDouble(array[7]));
+				obj.setVolume(Double.parseDouble(array[8]));
+				obj.setVolumePrice(Double.parseDouble(array[9]));
+				obj.setBuy1Num(Double.parseDouble(array[10]));
+				obj.setBuy1Pricae(Double.parseDouble(array[11]));
+				obj.setBuy2Num(Double.parseDouble(array[12]));
+				obj.setBuy2Pricae(Double.parseDouble(array[13]));
+				obj.setBuy3Num(Double.parseDouble(array[14]));
+				obj.setBuy3Pricae(Double.parseDouble(array[15]));
+				obj.setBuy4Num(Double.parseDouble(array[16]));
+				obj.setBuy4Pricae(Double.parseDouble(array[17]));
+				obj.setBuy5Num(Double.parseDouble(array[18]));
+				obj.setBuy5Pricae(Double.parseDouble(array[19]));
+				obj.setSell1Num(Double.parseDouble(array[20]));
+				obj.setSell1Pricae(Double.parseDouble(array[21]));
+				obj.setSell2Num(Double.parseDouble(array[22]));
+				obj.setSell2Pricae(Double.parseDouble(array[23]));
+				obj.setSell3Num(Double.parseDouble(array[24]));
+				obj.setSell3Pricae(Double.parseDouble(array[25]));
+				obj.setSell4Num(Double.parseDouble(array[26]));
+				obj.setSell4Pricae(Double.parseDouble(array[27]));
+				obj.setSell5Num(Double.parseDouble(array[28]));
+				obj.setSell5Pricae(Double.parseDouble(array[29]));
+				LocalDateTime ldt = Tools.string2LocalDateTime(array[30] + " " + array[31]);
+				obj.setDate(ldt.toLocalDate());
+				obj.setTime(ldt.toLocalTime());
+				result.add(obj);
+			} else {
+				Matcher indexMatcher = indexPatter.matcher(reresponseString);
+				if (indexMatcher.find()) {
+					RealTimeDataPOJO obj = new RealTimeDataPOJO();
+					obj.setType(RealTimeDataPOJO.INDEX);
+					obj.setFullCode(indexMatcher.group(1));
+					String[] array = indexMatcher.group(2).split(",");
+					obj.setName(array[0]);
+					obj.setNow(Double.parseDouble(array[1]));
+					obj.setRiseAndFall(Double.parseDouble(array[2]));
+					obj.setRiseAndFallPercent(Double.parseDouble(array[3]));
+					obj.setVolume(Double.parseDouble(array[4]));
+					obj.setVolumePrice(Double.parseDouble(array[5]));
+					LocalDateTime ldt = LocalDateTime.now();
+					obj.setDate(ldt.toLocalDate());
+					obj.setTime(ldt.toLocalTime());
+					result.add(obj);
+				}
+			}
+		}
+		return result;
+	}
 	/**
 	 * 获取股票历史数据
 	 * 例子：<br>
